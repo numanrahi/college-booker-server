58daa2322da6ef04773da16a026e755c1e7274eb
numanrahi <arahi2994@gmail.com>
2023-07-24 18:42:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://collegeBooker:dAS8LOMtkRreWHJT@cluster0.vfido6v.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const collegeCollection = client.db("collegeBooker").collection("colleges");
+const reviewCollection = client.db("collegeBooker").collection("review");
+
+async function run() {
+  try {
+    app.get("/colleges", async (req, res) => {
+      const cursor = collegeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("college booker is running");
+});
+
+app.listen(port, () => {
+  console.log(`college booker is running on port: ${port}`);
+});
