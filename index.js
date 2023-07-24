1c08b749af66625e6407b24dfdcac40ddd61616b
numanrahi <arahi2994@gmail.com>
2023-07-24 22:40:01 +0600

    college id api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,6 +29,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get("/colleges/:ID", async (req, res) => {
+      const id = req.params.ID;
+      const query = { _id: new ObjectId(id) }
+      const result = await collegeCollection.findOne(query);
+      res.send(result);
+  })
 
     app.get("/reviews", async (req, res) => {
       const cursor = reviewCollection.find();
